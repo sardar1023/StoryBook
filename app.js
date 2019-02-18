de32f133112f8597c6a21dd2c1a8f1c1c8725e8c
sardar yalkun <sardaryalkun1023@gmail.com>
2019-02-17 20:34:39 -0500

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('It works...');
+});
+
 const port = process.env.PORT || 5000; 
 
 app.listen(port, ()=>{
